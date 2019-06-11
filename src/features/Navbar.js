456133cd3ca3c670149c4d86a72ee3d88011db1f
fernandogelin <fernandogelin@gmail.com>
2019-06-11 17:03:08 -0400

    feat(init): initial commit, set up storybook, add navbar component, override bootstratp default colors

---
+++ src/features/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { ReactComponent as CCVLogo } from '../assets/svg/ccv-logo.svg';
+import { ReactComponent as BrownLogo } from '../assets/svg/brown-logo.svg';
+import '../styles/custom.scss';
+
+const Navbar = () => (
+    <section className="p-2 pl-10 brown-nav bg-white">
+        <a href="https://www.brown.edu">
+            <BrownLogo />
+        </a>
+        <a href="https://ccv.brown.edu">
+            <CCVLogo />
+        </a>
+    </section>
+);
+
+export default Navbar;
