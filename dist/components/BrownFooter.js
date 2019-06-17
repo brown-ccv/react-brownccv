15ccfa5bcfb5b19f700e7a4394a5107d1cf32d45
fernandogelin <fernandogelin@gmail.com>
2019-06-17 14:26:27 -0400

    feat(babel): adds babel to compile es6

---
+++ dist/components/BrownFooter.js
@@ -0,0 +1,146 @@
+import React from 'react';
+
+var BrownFooterLogo = function BrownFooterLogo(props) {
+  return React.createElement("svg", props, React.createElement("title", null, "logo"), React.createElement("g", {
+    id: "Designs-v2",
+    stroke: "none",
+    strokeWidth: "1",
+    fill: "none",
+    fillRule: "evenodd"
+  }, React.createElement("g", {
+    id: "Page-1-Features",
+    transform: "translate(-17 -1769)",
+    fill: "#FEFEFE"
+  }, React.createElement("g", {
+    id: "Group-8",
+    transform: "translate(0 1727)"
+  }, React.createElement("g", {
+    id: "logo",
+    transform: "translate(17 42)"
+  }, React.createElement("path", {
+    d: "M7.213 13.831v8.934c0 2.747.533 3.689 3.525 3.648 3.443 0 6.352-1.721 6.352-6.065 0-4.468-3.278-6.517-7.5-6.517H7.213zm0-1.189h2.583c3.934 0 5.9-1.844 5.9-5.204 0-2.46-1.024-5.205-5.86-5.205-1.352 0-1.926.123-2.254.245-.246.083-.369.246-.369 1.066v9.098zM11.23 1.044c2.952 0 4.632.492 5.984 1.434 1.394.984 2.132 2.46 2.132 4.509 0 3.606-3.402 5.369-5.37 5.86v.082c3.73.533 7.09 2.746 7.09 6.886 0 2.91-1.434 4.958-3.81 6.312-2.09 1.147-4.714 1.557-7.829 1.557H0v-.943c3.443-.246 3.77-.532 3.77-5V6.987c-.04-4.467-.286-4.754-3.524-5v-.943h10.983z",
+    id: "Fill-1"
+  }), React.createElement("path", {
+    d: "M35.328 14.774h2.377c1.681 0 2.992-.205 4.1-1.107 1.762-1.393 2.377-3.401 2.377-5.41 0-4.303-3.116-6.024-6.19-6.024-1.393 0-1.966.082-2.294.205-.246.082-.37.286-.37 1.106v11.23zm0 6.967c0 4.468.328 4.754 3.689 5v.943H28.115v-.943c3.443-.246 3.77-.532 3.77-5V6.987c0-4.467-.327-4.754-3.606-5v-.942h10.41c2.623 0 4.96.367 6.476 1.393 1.64 1.024 2.704 2.868 2.704 5.245 0 3.402-2.09 5.82-5.409 7.173.737 1.23 2.418 4.057 3.648 5.86 1.475 2.172 2.335 3.28 3.442 4.509.86 1.024 1.598 1.68 3.074 2.05l-.082.778h-.574c-4.713-.124-6.148-1.558-7.705-3.812-1.271-1.844-2.992-4.877-4.14-6.804-.655-1.065-1.311-1.475-2.582-1.475h-2.213v5.78z",
+    id: "Fill-4"
+  }), React.createElement("path", {
+    d: "M70.427 1.782c-4.837 0-8.934 3.975-8.934 11.68 0 7.91 4.139 13.484 10.123 13.484 4.918 0 8.934-3.811 8.934-11.435 0-8.852-4.263-13.73-10.123-13.73m14.099 12.215c0 8.523-5.943 14.302-13.77 14.302-7.747 0-13.239-5.901-13.239-13.647 0-6.886 4.836-14.222 13.976-14.222 7.09 0 13.033 5.492 13.033 13.567",
+    id: "Fill-6"
+  }), React.createElement("path", {
+    d: "M98.534 1.987l-1.272.164c-1.844.246-2.089.614-1.516 2.418l5.533 17.459h.082l6.558-20.574h.983l7.42 20.533h.08c1.68-5.164 4.057-13.812 4.755-16.763.574-2.377.37-2.868-1.476-3.073l-1.188-.164v-.942h8.688v.942c-2.704.328-2.992.491-4.344 4.221-.37 1.025-3.36 10.451-6.803 21.721h-1.106L107.55 7.89l-.082.081-6.68 19.96H99.64L92.55 5.92c-1.107-3.401-1.721-3.688-4.058-3.934v-.942h10.042v.942z",
+    id: "Fill-9"
+  }), React.createElement("path", {
+    d: "M157.44 27.97h-1.066L138.587 6.372h-.082v10.45c0 4.305.164 6.518.329 7.788.205 1.434 1.311 2.008 4.016 2.132v.942h-9.836v-.942c2.254-.083 3.32-.698 3.524-2.132.165-1.27.328-3.483.328-7.787V7.929c0-2.95-.041-3.607-.778-4.548-.78-.984-1.886-1.271-3.608-1.394v-.943h5.984L155.72 21.7h.082v-9.795c0-4.303-.164-6.517-.328-7.787-.205-1.435-1.312-2.008-4.016-2.131v-.943h9.836v.943c-2.254.082-3.32.696-3.525 2.13-.164 1.271-.327 3.485-.327 7.788V27.97z",
+    id: "Fill-11"
+  }))))));
+};
+
+BrownFooterLogo.defaultProps = {
+  width: "162",
+  height: "29",
+  viewBox: "0 0 162 29",
+  version: "1.1",
+  xmlns: "http://www.w3.org/2000/svg"
+};
+import '../styles/custom.scss';
+var currentYear = new Date().getFullYear();
+
+var BrownFooter = function BrownFooter() {
+  return React.createElement("footer", {
+    id: "brown-footer",
+    className: "d-print-none"
+  }, React.createElement("div", {
+    className: "marginator"
+  }, React.createElement("section", null, React.createElement(BrownFooterLogo, null), React.createElement("div", {
+    id: "give-to-brown"
+  }, React.createElement("a", {
+    href: "http://giving.brown.edu",
+    className: "giving-footer"
+  }, "Giving to Brown"))), React.createElement("div", {
+    id: "brown-contact"
+  }, React.createElement("ul", {
+    className: "contact-location"
+  }, React.createElement("li", {
+    className: "city-state-country i-destination"
+  }, "Providence, Rhode Island 02912, USA"), React.createElement("li", {
+    className: "telephone i-phone"
+  }, React.createElement("a", {
+    href: "tel:14018631000"
+  }, "401-863-1000")), React.createElement("li", {
+    className: "contact i-devices"
+  }, React.createElement("a", {
+    href: "http://www.brown.edu/contact",
+    accessKey: "9"
+  }, "Contact Us")), React.createElement("li", {
+    className: "maps i-map"
+  }, React.createElement("a", {
+    href: "http://www.brown.edu/Facilities/Facilities_Management/maps/#building/"
+  }, "Maps")), React.createElement("li", {
+    className: "directions i-split"
+  }, React.createElement("a", {
+    href: "http://www.brown.edu/about/visit/driving-directions"
+  }, "Directions")))), React.createElement("div", {
+    id: "social-media"
+  }, React.createElement("ul", null, React.createElement("li", null, React.createElement("a", {
+    href: "https://www.facebook.com/BrownUniversity",
+    title: "Facebook"
+  }, React.createElement("i", {
+    className: "i-facebook",
+    "aria-hidden": "true"
+  }))), React.createElement("li", null, React.createElement("a", {
+    href: "https://twitter.com/BrownUniversity"
+  }, React.createElement("i", {
+    className: "i-twitter",
+    "aria-hidden": "true"
+  }))), React.createElement("li", null, React.createElement("a", {
+    href: "http://www.youtube.com/brownuniversity"
+  }, React.createElement("i", {
+    className: "i-youtube",
+    "aria-hidden": "true"
+  }))), React.createElement("li", null, React.createElement("a", {
+    href: "http://instagram.com/brownu"
+  }, React.createElement("i", {
+    className: "i-instagram",
+    "aria-hidden": "true"
+  }))), React.createElement("li", null, React.createElement("a", {
+    href: "http://itunes.apple.com/institution/brown-university/id381076688#ls=1"
+  }, React.createElement("i", {
+    className: "i-apple",
+    "aria-hidden": "true"
+  }))), React.createElement("li", null, React.createElement("a", {
+    href: "https://plus.google.com/100931085598361161047/posts"
+  }, React.createElement("i", {
+    className: "i-google-plus",
+    "aria-hidden": "true"
+  }))), React.createElement("li", null, React.createElement("a", {
+    href: "https://www.linkedin.com/edu/school?id=19348&trk=tyah"
+  }, React.createElement("i", {
+    className: "i-linkedin",
+    "aria-hidden": "true"
+  }))), React.createElement("li", null, React.createElement("a", {
+    href: "https://www.snapchat.com/add/brown-u"
+  }, React.createElement("i", {
+    className: "i-snapchat",
+    "aria-hidden": "true"
+  })))), React.createElement("div", {
+    className: "social-link"
+  }, React.createElement("a", {
+    href: "http://www.brown.edu/about/social-media/",
+    title: "Get Connected to the Brown Community"
+  }, "All Social Media")))), React.createElement("div", {
+    id: "footer-copyright",
+    className: "clearfix"
+  }, React.createElement("div", {
+    className: "marginator"
+  }, React.createElement("p", null, React.createElement("span", {
+    className: "copyright"
+  }, "\xA9 ", currentYear, " Brown University"), " | ", React.createElement("a", {
+    href: "#",
+    id: "back-to-top",
+    className: "link-top"
+  }, "Back to top ", React.createElement("span", {
+    className: "icon i-arrow-up-line"
+  }))))));
+};
+
+export default BrownFooter;
---
+++ src/stories/spinner.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Spinner from "../components/Spinner";
+
+storiesOf("Spinner", module).add("Spinner", () => <Spinner />);
